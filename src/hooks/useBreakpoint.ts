0ae8dbc6f0372c0f3614b53b6d452eabdfde236a
Adrian Sudak <adriansudak@outlook.com>
2024-08-18 00:03:28 +0200

    fix: Set correct breakpoint on load

--- src/hooks/useBreakpoint.ts
+++ src/hooks/useBreakpoint.ts
@@ -1,36 +1,37 @@
-import { useEffect, useState } from 'react'
-import { pixelsToNumber } from '../utils/pixels';
-import { breakpoints} from '../styles/breakpoints';
+import { useEffect, useState } from "react";
+import { pixelsToNumber } from "../utils/pixels";
+import { breakpoints } from "../styles/breakpoints";
 
 const useBreakpoint = () => {
   const [breakpoint, setBreakpoint] = useState<string>(breakpoints.md);
 
-  useEffect(()=> {
+  useEffect(() => {
     const handleBreakpoint = () => {
-      let breakpoint = '';
+      let breakpoint = "";
       const width = window.innerWidth;
-      
-      if(width < (pixelsToNumber(breakpoints.xs) || 0))
-        breakpoint = breakpoints.xs
-      else if(width < (pixelsToNumber(breakpoints.sm) || 0))
-        breakpoint = breakpoints.sm
-      else if(width < (pixelsToNumber(breakpoints.md) || 0))
-        breakpoint = breakpoints.md
-      else if(width < (pixelsToNumber(breakpoints.lg) || 0))
-        breakpoint = breakpoints.lg
-      else if(width < (pixelsToNumber(breakpoints.xl) || 0))
-        breakpoint = breakpoints.xl
-      else 
-        breakpoint = breakpoints.xxl
-      
-      setBreakpoint(breakpoint);
-    }
 
-    window.addEventListener('resize', handleBreakpoint);
-    return () => window.removeEventListener('resize', handleBreakpoint);
-  }, [])
+      if (width < (pixelsToNumber(breakpoints.xs) || 0))
+        breakpoint = breakpoints.xs;
+      else if (width < (pixelsToNumber(breakpoints.sm) || 0))
+        breakpoint = breakpoints.sm;
+      else if (width < (pixelsToNumber(breakpoints.md) || 0))
+        breakpoint = breakpoints.md;
+      else if (width < (pixelsToNumber(breakpoints.lg) || 0))
+        breakpoint = breakpoints.lg;
+      else if (width < (pixelsToNumber(breakpoints.xl) || 0))
+        breakpoint = breakpoints.xl;
+      else breakpoint = breakpoints.xxl;
+
+      setBreakpoint(breakpoint);
+    };
+
+    handleBreakpoint();
+
+    window.addEventListener("resize", handleBreakpoint);
+    return () => window.removeEventListener("resize", handleBreakpoint);
+  }, []);
 
   return breakpoint;
-}
+};
 
-export default useBreakpoint
+export default useBreakpoint;
