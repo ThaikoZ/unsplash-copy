bdeff529eda12b1fc6704929f869edf6bd32ab61
Adrian Sudak <adriansudak@outlook.com>
2024-08-20 00:48:38 +0200

    style: Style SearchPage elements spacing

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -19,18 +19,9 @@ const TagContainer = styled.div`
   display: flex;
   gap: 0.5rem;
   padding: 0 1.25rem;
-  margin: 0 1rem 0 0;
-  margin-bottom: 1.5rem;
+  margin: 1.5rem 1.25rem 2rem 0;
   overflow: hidden;
   flex-wrap: nowrap;
-
-  &::after {
-    position: absolute;
-    right: 0;
-    width: 50px;
-    height: 25px;
-    background-color: red;
-  }
 `;
 
 const StyledLink = styled(Link)`
@@ -39,11 +30,11 @@ const StyledLink = styled(Link)`
   align-items: center;
   min-width: 100px;
   font-weight: 600;
-  font-size: 0.95rem;
+  font-size: 0.9rem;
   height: 25px;
   border-radius: 7px;
   border: 1px solid rgba(0, 0, 0, 0.15);
-  color: rgba(0, 0, 0, 0.45);
+  color: rgba(0, 0, 0, 0.55);
   padding: 0.5rem 1.5rem;
   cursor: pointer;
   text-decoration: none;
@@ -62,15 +53,15 @@ const StyledLink = styled(Link)`
 `;
 
 const tags = [
-  "luxury",
-  "cars",
-  "Catalonia",
-  "houses",
-  "bitches",
-  "lovers",
-  "flowers",
-  "helped",
-  "gracias",
+  "Luxury",
+  "Porshe",
+  "Apple",
+  "Poland",
+  "Beach",
+  "Deep house",
+  "Horses",
+  "El Cubano",
+  "USA",
 ];
 
 const SearchPage = () => {
