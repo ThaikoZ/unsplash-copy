cecddc8da7d06d6fd56ec5c5765f4bc09721a79f
Adrian Sudak <adriansudak@outlook.com>
2024-08-19 12:18:21 +0200

    style: Header

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,6 +1,18 @@
+import { styled } from "styled-components";
 import Container from "../components/layout/Container";
 import PhotoGrid from "../components/PhotoGrid";
 import { searchRoute } from "../routes";
+
+const Header = styled.h1`
+  font-size: 1.75rem;
+  margin-top: 3rem;
+  font-weight: 700;
+  padding: 0 1.25rem;
+
+  &::first-letter {
+    text-transform: capitalize;
+  }
+`;
 
 const SearchPage = () => {
   const { query } = searchRoute.useParams();
@@ -9,7 +21,7 @@ const SearchPage = () => {
   return (
     <Container>
       {/* TODO: Show Header */}
-      <p>{query}</p>
+      <Header>{query}</Header>
       {/* TODO: Show tags */}
       <PhotoGrid searchQuery={query} />
     </Container>
